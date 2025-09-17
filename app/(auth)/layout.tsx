5f4717e5c3bde43f0cf9c9704aa1bc17a6424fab
CardosoRogerio <157244722+CardosoRogerio@users.noreply.github.com>
2025-09-17 14:56:42 -0300

    fix: add layout for (auth) to fix Next.js build

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className="bg-gray-50 flex items-center justify-center min-h-screen">
+        {children}
+      </body>
+    </html>
+  );
+}
